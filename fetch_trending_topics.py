90664daabbe7a654213ba1a00e79c56acb3702a2
cont2rani7 <cont2rani7@gmail.com>
2025-08-02 23:56:03 +0530

    Update fetch_trending_topics.py

--- fetch_trending_topics.py
+++ fetch_trending_topics.py
@@ -1,7 +1,8 @@
 import feedparser
 
 def get_trending_topics():
-    feed_url = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=IN"
+    #feed_url = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=IN"
+    feed_url = "https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en"
     feed = feedparser.parse(feed_url)
     topics = []
     for entry in feed.entries:
